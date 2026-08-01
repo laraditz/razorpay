52ff3e060267c7af48e84675975281c725408ca8
Raditz Farhan <raditzfarhan@gmail.com>
2026-08-02 06:47:42 +0800

    fix: Rearrange payment_id added to orders and payment_links tables

--- database/migrations/2026_08_03_000000_add_payment_id_to_razorpay_orders_table.php
+++ database/migrations/2026_08_03_000000_add_payment_id_to_razorpay_orders_table.php
@@ -4,12 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::table('razorpay_orders', function (Blueprint $table) {
-            $table->string('payment_id')->nullable()->index();
+            $table->string('payment_id')->nullable()->index()->after('razorpay_id');
         });
     }
 
