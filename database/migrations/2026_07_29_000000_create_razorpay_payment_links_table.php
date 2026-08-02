7bbe519a53b8624a24a36dd934082d16a6d4ae78
Raditz Farhan <raditzfarhan@gmail.com>
2026-08-02 10:31:53 +0800

    refactor: Add payment_id to razorpay_payment_links and razorpay_orders table and deleted individual migrations

--- database/migrations/2026_07_29_000000_create_razorpay_payment_links_table.php
+++ database/migrations/2026_07_29_000000_create_razorpay_payment_links_table.php
@@ -4,13 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('razorpay_payment_links', function (Blueprint $table) {
             $table->id();
             $table->string('razorpay_id')->unique();
+            $table->string('payment_id')->nullable()->index();
             $table->string('order_id')->nullable()->index();
             $table->string('status')->index();
 
